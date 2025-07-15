0cceb763b222cf17aeefafffeadfa597727f9999
mebaran <mebaran@gmail.com>
2025-07-15 18:35:11 +0000

    Added podman and cuda support

--- wsl/configuration.nix
+++ wsl/configuration.nix
@@ -1,43 +1,88 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     # include NixOS-WSL modules
     <nixos-wsl/modules>
   ];
 
+  #wsl config
   wsl.enable = true;
   wsl.defaultUser = "mebaran";
   wsl.interop.includePath = false;
   wsl.interop.register = true;
   wsl.useWindowsDriver = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "nvidia-x11"
-  ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nixpkgs.config.allowUnfree = true;
+
+  #cuda
   hardware = {
     nvidia-container-toolkit.enable = true;
     nvidia = {
       open = true;
       nvidiaSettings = false;
     };
+    graphics.enable = true;
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = ["nvidia"];
+  environment.sessionVariables = {
+    CUDA_PATH = "${pkgs.cudatoolkit}";
+    EXTRA_LDFLAGS = "-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib";
+    EXTRA_CCFLAGS = "-I/usr/include";
+    LD_LIBRARY_PATH = [
+      "/usr/lib/wsl/lib"
+      "${pkgs.linuxPackages.nvidia_x11}/lib"
+      "${pkgs.ncurses5}/lib"
+    ];
+    MESA_D3D12_DEFAULT_ADAPTER_NAME = "Nvidia";
+  };
+  systemd.services = {
+    nvidia-cdi-generator = {
+      description = "Generate nvidia cdi";
+      wantedBy = ["podman.service"];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.nvidia-docker}/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml --nvidia-ctk-path=${pkgs.nvidia-container-toolkit}/bin/nvidia-ctk";
+      };
+    };
+  };
+  # Enable common container config files in /etc/containers
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
 
-  programs.nix-ld.enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
 
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  #userspace
+  programs = {
+    git.enable = true;
+    nix-ld.enable = true;
+    zsh.enable = true;
+  };
   environment.systemPackages = [
     pkgs.wl-clipboard
+    pkgs.vim
+    pkgs.home-manager
+    pkgs.podman
   ];
 
+  users.defaultUserShell = pkgs.zsh;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
